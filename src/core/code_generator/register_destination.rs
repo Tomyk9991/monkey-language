6c54efecd3b137c1d2ac10e3af9df8e59c877b05
Thomas <tomyk9991@web.de>
2023-09-29 17:35:09 +0200

    * raw pointers are base a base of the type system now. (WIP)

---
+++ src/core/code_generator/register_destination.rs
@@ -0,0 +1,16 @@
+/// Returns the responsible register according to the provided byte size
+/// # Example
+///  - byte = 8 => rax
+///  - byte = 4 => rax
+///  - byte = 2 => ax
+///  - byte = 1 => al
+pub fn from_byte_size(byte: usize) -> String {
+    return format!("{}", match byte {
+        8 => "rax",
+        4 => "eax",
+        2 => "ax",
+        1 => "al",
+        0 => "eax",
+        _ => "undefined byte"
+    });
+}
