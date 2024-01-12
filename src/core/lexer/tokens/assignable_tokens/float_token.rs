764680fef9f6dc7fe856f1c84c61d1ba4a9357e4
Thomas <tomyk9991@web.de>
2024-01-12 18:07:11 +0100

    added pastebin

--- src/core/lexer/tokens/assignable_tokens/float_token.rs
+++ src/core/lexer/tokens/assignable_tokens/float_token.rs
@@ -7,6 +7,7 @@ use crate::core::lexer::type_token::Float;
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct FloatToken {
+    // https://pastebin.com/DWcHQbT5
     // there is no need to use a string literal instead of a f64 like in the integer token, because
     // you cant have a float that's bigger than the biggest value of f64. but you can have a bigger value than a i64. consider every number that's between i64::MAX and u64::MAX
     pub value: f64,
